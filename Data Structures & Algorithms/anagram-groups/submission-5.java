83ca08210260be75b8fe4699bfc084f71c7d2150
AyanKS <saha9764.ayan@gmail.com>
2026-06-19 01:32:07 +0530

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        final int n = strs.length;
+        final Map<Map<Character, Integer>, List<String>> groups = new HashMap<>(n);
+        Map<Character, Integer> key;
+        List<String> value;
+        String str;
+
+        for (int i = 0; i < n; i++) {
+            str = strs[i];
+            key = generateFrequencyMap(str);
+            value = groups.getOrDefault(key, new LinkedList<>());
+            value.add(str);
+            groups.put(key, value);
+        }
+
+        final List<List<String>> result = new LinkedList<>();
+        for (final List<String> groupVal : groups.values()) {
+            result.add(groupVal);
+        }
+        return result;
+    }
+
+    final Map<Character, Integer> generateFrequencyMap(final String str) {
+        final Map<Character, Integer> frequencyMap = new HashMap<>(26);
+        for (final char c : str.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        return frequencyMap;
+    }
+}
